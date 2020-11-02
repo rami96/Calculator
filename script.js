17ad3ad1d4bfdd6dd84b6edc169892e5f5a91b88
Saifeddine Rami <sifeddinerami96@gmail.com>
2020-11-02 18:25:36 +0100

    Updated bugs

--- script.js
+++ script.js
@@ -1,91 +1,123 @@
-let entering = "";
-let screen = document.getElementById('maintext');
-screen.innerText = entering;
-let len = 0;
-let b;
+const operators = {
+    ADD: "add",
+    SUBTRACT: "subtract",
+    MULTIPLY: "multiply",
+    DIVIDE: "divide"
+}
 
-//operations
-addition=(a,b)=>{
+var operator = "";
+var num1 = null;
+var num2 = null;
+var display = "";
+var readyForNum2 = false;
+
+function add(a, b) {
     return a + b;
 }
 
-substraction=(a,b)=>{
+function subtract(a, b) {
     return a - b;
 }
 
-multiplication=(a,b)=>{
+function multiply(a, b) {
     return a * b;
 }
 
-division=(a,b)=>{
+function divide(a, b) {
+    if (a == 0 || b == 0) {
+        alert('"Error." \n\n You divided by 0');
+        clearAll();
+        return 0;
+    }
     return a / b;
 }
 
-//Assign Numbers then show in screen when pressed
-let numbers = document.querySelectorAll(".btnnum");
-for (let num = 0; num < numbers.length; num++){
-    numbers[num].addEventListener('click',function(){
-        entering+=this.id;
-        screen.innerText = entering;
-        len++;
-    })
+function invert() {
+    display *= -1;
+    updateDisplay();
 }
 
-//Assign operators then show in screen when pressed
-let operators = document.querySelectorAll(".btnoperator");
-for (let op = 0; op < operators.length; op++){
-    operators[op].addEventListener('click',function(){
-        //alert("The pressed button is: " + this.id)
-        entering+=this.id;
-        screen.innerText = entering;
-        len++;
-    })
+function percentage() {
+    display /= 100;
+    updateDisplay();
 }
 
-//Assign "=" sign and calculate then show in screen
-let equals = document.getElementById("enter");
-equals.addEventListener('click',function(){//evel blast operate
-    entering === "" ? ("") : entering = operate(entering);
-    //screen.innerText = entering;
-})
+function bcksppace(){
+    display = display.substr(0,(display.length)-1);
+    updateDisplay();
+}
 
-//Assign "AC" and make it reset the screen
-let reset = document.getElementById("clear");
-reset.addEventListener('click',function(){
-    entering = "";
-    screen.innerText = entering;
-    len = 0;
-})
+function clearAll() {
+    operator = "";
+    num1 = null;
+    num2 = null;
+    readyForNum2 = false;
 
+    display = "0";
+    updateDisplay();
+}
 
-operate=(operation)=>{
-    //Knowing what sign is used in the operation;
-    let sign = "";
-    for(let oper = 0; oper < operation.length; oper++){
-        if(operation[oper] === "/" || operation[oper] === "*"|| 
-        operation[oper] === "-"|| operation[oper] === "+"){
-            sign = operation[oper];
+function operate() {
+    let result = 0;
+    if (num1 != null && operator != '') {
+        num2 = Number(display);
+
+        switch(operator) {
+            case operators.ADD:
+                result = add(num1, num2);
+            break;
+    
+            case operators.SUBTRACT:
+                result = subtract(num1, num2);
+            break;
+    
+            case operators.MULTIPLY:
+                result = multiply(num1, num2);
+            break;
+    
+            case operators.DIVIDE:
+                result = divide(num1, num2);
+            break;
         }
+
+        num2 = null;
+        operator = '';
+
+        display = result;
+    
+        num1 = result;
+    
+        updateDisplay();
     }
-    //getting the index of the sign
-    let index = operation.indexOf(sign);
-    //slicing the opperation into two sides, a and b
-    let a = operation.slice(0,index);
-    let b = operation.slice(index+1,operation.length)
-    //chosing the right operation for the sign
-    if(sign === '/') entering = division(a,b);
-    else if(sign === '*') entering = multiplication(a,b);
-    else if(sign === '-') entering = substraction(a,b);
-    else entering = addition(a,b);
-    //showing the result on the screen
-    screen.innerText = entering;
 }
-//Assign Backspace button and make it delete last entered number
-let back = document.getElementById("bck");
-back.addEventListener('click',function(){
-    if(entering){
-        entering = entering.substr(0,entering.length-1)
+
+function setNumber(num) {
+    if (!readyForNum2) {
+        if (display == "0") {
+            display = num;
+        } else {
+            display += num;
+        }
+    } else {
+        display = num;
+        readyForNum2 = false;
     }
-    //entering.substr(0,entering.length-1)
-    screen.innerText = entering;
-})
+
+    updateDisplay();
+}
+
+function setOperator(op) {
+    operator = op;
+
+    num1 = Number(display);
+
+    readyForNum2 = true;
+}
+
+function updateDisplay() {
+    display = display.toString();
+    if (display.length > 1 && display.substring(0,1) == '0') {
+        display = display.replace('0', '');
+    }
+    document.getElementById('display').textContent = display;
+}
